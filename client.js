783ff923f30f6a8de4cf62626768ee772f9456e6
Davisycompany <145684882+Davisycompany@users.noreply.github.com>
2023-10-06 10:38:03 +0700

    Update client.js

--- client.js
+++ client.js
@@ -25,7 +25,7 @@ const streamConstraints = { audio: true, video: true };
 
 //let socket = io.connect(`https://${LOCAL_IP_ADDRESS}`, {secure: true});
 // let socket = io.connect("http://localhost");
-let socket = io.connect("http://video-call-a95d.onrender.com:80");
+let socket = io.connect("https://video-call-a95d.onrender.com:443");
 
 //let socket = io.connect("http://3.125.183.140:8000");
 btnToggleVideo.addEventListener("click", () => toggleTrack("video"));
