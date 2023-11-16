af109c1a402f3f519dd4fb2a3faf2cfc3fe9f9dd
theanishtar <dangtt135@gmail.com>
2023-11-16 13:26:52 +0700

    xxxx

--- client.js
+++ client.js
@@ -1,189 +1,203 @@
-const LOCAL_IP_ADDRESS = "video-call-a95d.onrender.com"; // change it
+const LOCAL_IP_ADDRESS = "https://servercvvservercvv.hop.sh"; // change it
+
+
 
 const getElement = id => document.getElementById(id);
 const [btnConnect, btnToggleVideo, btnToggleAudio, divRoomConfig, roomDiv, roomNameInput, localVideo, remoteVideo] = ["btnConnect",
-	"toggleVideo", "toggleAudio", "roomConfig", "roomDiv", "roomName",
-	"localVideo", "remoteVideo"].map(getElement);
+    "toggleVideo", "toggleAudio", "roomConfig", "roomDiv", "roomName",
+    "localVideo", "remoteVideo"].map(getElement);
 let remoteDescriptionPromise, roomName, localStream, remoteStream,
-	rtcPeerConnection, isCaller;
+    rtcPeerConnection, isCaller;
 
 // you can use public stun and turn servers,
 // but we don't need for local development
 const iceServers = {
-	iceServers: [
-		{ 'urls': 'stun:stun.stunprotocol.org:3478' },
-		{ 'urls': 'stun:stun.l.google.com:19302' },
-	]
+    iceServers: [
+        { 'urls': 'stun:stun.stunprotocol.org:3478' },
+        { 'urls': 'stun:stun.l.google.com:19302' },
+    ]
 };
-
 
 const streamConstraints = { audio: true, video: true };
 
-// let socket = io.connect(`https://${LOCAL_IP_ADDRESS}`, { secure: true });
-// let socket = io.connect("http://localhost");
-let socket = io.connect("https://video-call-a95d.onrender.com:443");
+//let socket = io.connect(`https://${LOCAL_IP_ADDRESS}`, {secure: true});
+//let socket = io.connect("https://0.0.0.0:8080");
+//let socket = io.connect("https://servercvvservercvv.hop.sh");
 
-//let socket = io.connect("http://3.125.183.140:8000");
+
+// Kết nối với server
+const socket = io('https://vnjrbdujf.onrender.com:443');
+
+// Lắng nghe sự kiện 'connect'
+socket.on('connect', () => {
+    alert("success")
+});
+// Lắng nghe sự kiện lỗi
+// socket.on('connect_error', (error) => {
+//     alert('Lỗi kết nối:', error.message);
+// });
+
+
 btnToggleVideo.addEventListener("click", () => toggleTrack("video"));
 btnToggleAudio.addEventListener("click", () => toggleTrack("audio"));
 
 function toggleTrack(trackType) {
-	if (!localStream) {
-		return;
-	}
+    if (!localStream) {
+        return;
+    }
 
-	const track = trackType === "video" ? localStream.getVideoTracks()[0]
-		: localStream.getAudioTracks()[0];
-	const enabled = !track.enabled;
-	track.enabled = enabled;
+    const track = trackType === "video" ? localStream.getVideoTracks()[0]
+        : localStream.getAudioTracks()[0];
+    const enabled = !track.enabled;
+    track.enabled = enabled;
 
-	const toggleButton = getElement(
-		`toggle${trackType.charAt(0).toUpperCase() + trackType.slice(1)}`);
-	const icon = getElement(`${trackType}Icon`);
-	toggleButton.classList.toggle("disabled-style", !enabled);
-	toggleButton.classList.toggle("enabled-style", enabled);
-	icon.classList.toggle("bi-camera-video-fill",
-		trackType === "video" && enabled);
-	icon.classList.toggle("bi-camera-video-off-fill",
-		trackType === "video" && !enabled);
-	icon.classList.toggle("bi-mic-fill", trackType === "audio" && enabled);
-	icon.classList.toggle("bi-mic-mute-fill", trackType === "audio" && !enabled);
+    const toggleButton = getElement(
+        `toggle${trackType.charAt(0).toUpperCase() + trackType.slice(1)}`);
+    const icon = getElement(`${trackType}Icon`);
+    toggleButton.classList.toggle("disabled-style", !enabled);
+    toggleButton.classList.toggle("enabled-style", enabled);
+    icon.classList.toggle("bi-camera-video-fill",
+        trackType === "video" && enabled);
+    icon.classList.toggle("bi-camera-video-off-fill",
+        trackType === "video" && !enabled);
+    icon.classList.toggle("bi-mic-fill", trackType === "audio" && enabled);
+    icon.classList.toggle("bi-mic-mute-fill", trackType === "audio" && !enabled);
 }
 
 btnConnect.onclick = () => {
-	if (roomNameInput.value === "") {
-		alert("Room can not be null!");
-	} else {
-		roomName = roomNameInput.value;
-		socket.emit("joinRoom", roomName);
-		divRoomConfig.classList.add("d-none");
-		roomDiv.classList.remove("d-none");
-	}
+    if (roomNameInput.value === "") {
+        alert("Room can not be null!");
+    } else {
+        roomName = roomNameInput.value;
+        socket.emit("joinRoom", roomName);
+        divRoomConfig.classList.add("d-none");
+        roomDiv.classList.remove("d-none");
+    }
 };
 
 const handleSocketEvent = (eventName, callback) => socket.on(eventName,
-	callback);
+    callback);
 
 handleSocketEvent("created", e => {
-	navigator.mediaDevices.getUserMedia(streamConstraints).then(stream => {
-		localStream = stream;
-		localVideo.srcObject = stream;
-		isCaller = true;
-	}).catch(console.error);
+    navigator.mediaDevices.getUserMedia(streamConstraints).then(stream => {
+        localStream = stream;
+        localVideo.srcObject = stream;
+        isCaller = true;
+    }).catch(console.error);
 });
 
 handleSocketEvent("joined", e => {
-	navigator.mediaDevices.getUserMedia(streamConstraints).then(stream => {
-		localStream = stream;
-		localVideo.srcObject = stream;
-		socket.emit("ready", roomName);
-	}).catch(console.error);
+    navigator.mediaDevices.getUserMedia(streamConstraints).then(stream => {
+        localStream = stream;
+        localVideo.srcObject = stream;
+        socket.emit("ready", roomName);
+    }).catch(console.error);
 });
 
 handleSocketEvent("candidate", e => {
-	if (rtcPeerConnection) {
-		const candidate = new RTCIceCandidate({
-			sdpMLineIndex: e.label, candidate: e.candidate,
-		});
+    if (rtcPeerConnection) {
+        const candidate = new RTCIceCandidate({
+            sdpMLineIndex: e.label, candidate: e.candidate,
+        });
 
-		rtcPeerConnection.onicecandidateerror = (error) => {
-			console.error("Error adding ICE candidate: ", error);
-		};
+        rtcPeerConnection.onicecandidateerror = (error) => {
+            console.error("Error adding ICE candidate: ", error);
+        };
 
-		if (remoteDescriptionPromise) {
-			remoteDescriptionPromise
-				.then(() => {
-					if (candidate != null) {
-						return rtcPeerConnection.addIceCandidate(candidate);
-					}
-				})
-				.catch(error => console.log(
-					"Error adding ICE candidate after remote description: ", error));
-		}
-	}
+        if (remoteDescriptionPromise) {
+            remoteDescriptionPromise
+                .then(() => {
+                    if (candidate != null) {
+                        return rtcPeerConnection.addIceCandidate(candidate);
+                    }
+                })
+                .catch(error => console.log(
+                    "Error adding ICE candidate after remote description: ", error));
+        }
+    }
 });
 
 handleSocketEvent("ready", e => {
-	if (isCaller) {
-		rtcPeerConnection = new RTCPeerConnection(iceServers);
-		rtcPeerConnection.onicecandidate = onIceCandidate;
-		rtcPeerConnection.ontrack = onAddStream;
-		rtcPeerConnection.addTrack(localStream.getTracks()[0], localStream);
-		rtcPeerConnection.addTrack(localStream.getTracks()[1], localStream);
-		rtcPeerConnection
-			.createOffer()
-			.then(sessionDescription => {
-				rtcPeerConnection.setLocalDescription(sessionDescription);
-				socket.emit("offer", {
-					type: "offer", sdp: sessionDescription, room: roomName,
-				});
-			})
-			.catch(error => console.log(error));
-	}
+    if (isCaller) {
+        rtcPeerConnection = new RTCPeerConnection(iceServers);
+        rtcPeerConnection.onicecandidate = onIceCandidate;
+        rtcPeerConnection.ontrack = onAddStream;
+        rtcPeerConnection.addTrack(localStream.getTracks()[0], localStream);
+        rtcPeerConnection.addTrack(localStream.getTracks()[1], localStream);
+        rtcPeerConnection
+            .createOffer()
+            .then(sessionDescription => {
+                rtcPeerConnection.setLocalDescription(sessionDescription);
+                socket.emit("offer", {
+                    type: "offer", sdp: sessionDescription, room: roomName,
+                });
+            })
+            .catch(error => console.log(error));
+    }
 });
 
 handleSocketEvent("offer", e => {
-	if (!isCaller) {
-		rtcPeerConnection = new RTCPeerConnection(iceServers);
-		rtcPeerConnection.onicecandidate = onIceCandidate;
-		rtcPeerConnection.ontrack = onAddStream;
-		rtcPeerConnection.addTrack(localStream.getTracks()[0], localStream);
-		rtcPeerConnection.addTrack(localStream.getTracks()[1], localStream);
+    if (!isCaller) {
+        rtcPeerConnection = new RTCPeerConnection(iceServers);
+        rtcPeerConnection.onicecandidate = onIceCandidate;
+        rtcPeerConnection.ontrack = onAddStream;
+        rtcPeerConnection.addTrack(localStream.getTracks()[0], localStream);
+        rtcPeerConnection.addTrack(localStream.getTracks()[1], localStream);
 
-		if (rtcPeerConnection.signalingState === "stable") {
-			remoteDescriptionPromise = rtcPeerConnection.setRemoteDescription(
-				new RTCSessionDescription(e));
-			remoteDescriptionPromise
-				.then(() => {
-					return rtcPeerConnection.createAnswer();
-				})
-				.then(sessionDescription => {
-					rtcPeerConnection.setLocalDescription(sessionDescription);
-					socket.emit("answer", {
-						type: "answer", sdp: sessionDescription, room: roomName,
-					});
-				})
-				.catch(error => console.log(error));
-		}
-	}
+        if (rtcPeerConnection.signalingState === "stable") {
+            remoteDescriptionPromise = rtcPeerConnection.setRemoteDescription(
+                new RTCSessionDescription(e));
+            remoteDescriptionPromise
+                .then(() => {
+                    return rtcPeerConnection.createAnswer();
+                })
+                .then(sessionDescription => {
+                    rtcPeerConnection.setLocalDescription(sessionDescription);
+                    socket.emit("answer", {
+                        type: "answer", sdp: sessionDescription, room: roomName,
+                    });
+                })
+                .catch(error => console.log(error));
+        }
+    }
 });
 
 handleSocketEvent("answer", e => {
-	if (isCaller && rtcPeerConnection.signalingState === "have-local-offer") {
-		remoteDescriptionPromise = rtcPeerConnection.setRemoteDescription(
-			new RTCSessionDescription(e));
-		remoteDescriptionPromise.catch(error => console.log(error));
-	}
+    if (isCaller && rtcPeerConnection.signalingState === "have-local-offer") {
+        remoteDescriptionPromise = rtcPeerConnection.setRemoteDescription(
+            new RTCSessionDescription(e));
+        remoteDescriptionPromise.catch(error => console.log(error));
+    }
 });
 
 handleSocketEvent("userDisconnected", (e) => {
-	remoteVideo.srcObject = null;
-	isCaller = true;
+    remoteVideo.srcObject = null;
+    isCaller = true;
 });
 
 handleSocketEvent("setCaller", callerId => {
-	isCaller = socket.id === callerId;
+    isCaller = socket.id === callerId;
 });
 
 handleSocketEvent("full", e => {
-	alert("room is full!");
-	window.location.reload();
+    alert("room is full!");
+    window.location.reload();
 });
 
 const onIceCandidate = e => {
-	if (e.candidate) {
-		console.log("sending ice candidate");
-		socket.emit("candidate", {
-			type: "candidate",
-			label: e.candidate.sdpMLineIndex,
-			id: e.candidate.sdpMid,
-			candidate: e.candidate.candidate,
-			room: roomName,
-		});
-	}
+    if (e.candidate) {
+        console.log("sending ice candidate");
+        socket.emit("candidate", {
+            type: "candidate",
+            label: e.candidate.sdpMLineIndex,
+            id: e.candidate.sdpMid,
+            candidate: e.candidate.candidate,
+            room: roomName,
+        });
+    }
 }
 
 const onAddStream = e => {
-	remoteVideo.srcObject = e.streams[0];
-	remoteStream = e.stream;
+    remoteVideo.srcObject = e.streams[0];
+    remoteStream = e.stream;
 }
